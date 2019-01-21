b596e9948aa53f742a460b0992e89d33d1893b76
Dhrubomoy Das Gupta <dhrubo_moy@yahoo.com>
2019-01-21 13:22:09 -0500

    minor

--- src/app/pages/twitter-analysis/twitter-analysis.module.ts
+++ src/app/pages/twitter-analysis/twitter-analysis.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { NgxEchartsModule } from 'ngx-echarts';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { ChartModule } from 'angular2-chartjs';
-
 import { ThemeModule } from '../../@theme/theme.module';
-
 import { TagComponent } from './tag/tag.component';
 import { AnalysisResultComponent } from './standard-search/analysis-result/analysis-result.component'
 import { AnalysisChartsComponent } from './standard-search/analysis-result/analysis-charts/analysis-charts.component'
-
 import { TwitterAnalysisRoutingModule, routedComponents } from './twitter-analysis-routing.module';
 import { StandardSearchAPIService } from './standard-search/standard-search-api.service'
 
